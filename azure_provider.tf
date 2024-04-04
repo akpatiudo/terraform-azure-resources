4491ab08ec035f68a44ff77170572b5bbe8c19ac
Ebenezer Akpati <118566096+akpatiudo@users.noreply.github.com>
2024-04-04 18:16:21 +0100

    Update azure_provider.tf credentials were updated

--- azure_provider.tf
+++ azure_provider.tf
@@ -1,8 +1,8 @@
 provider "azurerm" {
   skip_provider_registration = true
-  subscription_id = "679a5af8-c711-484b-865e-70076982e82f"
-  client_id       = "d0b88271-65c9-4eca-a34e-6c3caee916f0"
-  client_secret   = "bDH8Q~FgxBkeWGBMh~mr~B1PLxOOOS8psEGP7dq7"
-  tenant_id       = "2fda06a3-e8aa-49ef-bd11-bf41f2b56686"
+  subscription_id = "67##########"
+  client_id       = "############"
+  client_secret   = "#################"
+  tenant_id       = "#####################"
   features {}
 }
